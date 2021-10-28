943ed0330aa2c41115f0e62ab2b4c13791197d0f
st-huynguyen <huy.nguyen@supremetech.vn>
2021-10-28 09:12:06 +0700

    config prettier

--- web-frontend-basic/js/exercise1/script.js
+++ web-frontend-basic/js/exercise1/script.js
@@ -1,18 +1,18 @@
 // Input element
-const input = document.createElement("input");
-input.placeholder = "nhập năm sinh";
+const input = document.createElement('input');
+input.placeholder = 'nhập năm sinh';
 document.body.appendChild(input);
 
 // Button element
-const button = document.createElement("button");
-button.innerHTML = "Tính tuổi";
+const button = document.createElement('button');
+button.innerHTML = 'Tính tuổi';
 document.body.appendChild(button);
 
 // Calculate the age from input
 const eventHandler = function (event) {
   const x = 2021 - +input.value;
-  const p = document.createElement("p");
+  const p = document.createElement('p');
   p.innerHTML = `Tuổi của bạn là: ${x}`;
   document.body.appendChild(p);
 };
-button.addEventListener("click", eventHandler);
+button.addEventListener('click', eventHandler);
--- web-frontend-basic/js/exercise2/script.js
+++ web-frontend-basic/js/exercise2/script.js
@@ -1,114 +1,114 @@
 const data = [
   {
-    name: "Basic",
-    price: "$10 / month",
+    name: 'Basic',
+    price: '$10 / month',
     des: [
-      "10 users included",
-      "2GB of storage",
-      "Email support",
-      "Help center access",
+      '10 users included',
+      '2GB of storage',
+      'Email support',
+      'Help center access',
     ],
-    button: "Get Started",
+    button: 'Get Started',
   },
   {
-    name: "Pro",
-    price: "$30 / month",
+    name: 'Pro',
+    price: '$30 / month',
     des: [
-      "100 users included",
-      "20GB of storage",
-      "Priority email support",
-      "Help center access",
+      '100 users included',
+      '20GB of storage',
+      'Priority email support',
+      'Help center access',
     ],
-    button: "Buy Now",
+    button: 'Buy Now',
   },
 ];
 
 // Add font
-const link = document.createElement("link");
-link.setAttribute("rel", "stylesheet");
-link.setAttribute("type", "text/css");
+const link = document.createElement('link');
+link.setAttribute('rel', 'stylesheet');
+link.setAttribute('type', 'text/css');
 link.setAttribute(
-  "href",
-  "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+  'href',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap'
 );
 document.head.appendChild(link);
 
 // Body styles
 document.body.style.fontFamily = "'Roboto', sans-serif";
-document.body.style.fontSize = "16px";
+document.body.style.fontSize = '16px';
 
 // Create and style elements
-const containerE = document.createElement("div");
+const containerE = document.createElement('div');
 document.body.appendChild(containerE);
-containerE.style.textAlign = "center";
-containerE.style.maxWidth = "640px";
+containerE.style.textAlign = 'center';
+containerE.style.maxWidth = '640px';
 
-const planListE = document.createElement("ul");
+const planListE = document.createElement('ul');
 containerE.appendChild(planListE);
-planListE.style.display = "flex";
-planListE.style.gap = "20px";
-planListE.style.listStyle = "none";
-planListE.style.width = "100%";
+planListE.style.display = 'flex';
+planListE.style.gap = '20px';
+planListE.style.listStyle = 'none';
+planListE.style.width = '100%';
 planListE.style.padding = 0;
 
 data.forEach(function (item, index) {
-  const planItemE = document.createElement("li");
+  const planItemE = document.createElement('li');
   planListE.appendChild(planItemE);
-  planItemE.style.border = "1px solid #ddd";
-  planItemE.style.borderRadius = "3px";
-  planItemE.style.width = "50%";
-  planItemE.style.boxShadow = "1px 1px 5px rgba(0, 0, 0, 0.08)";
+  planItemE.style.border = '1px solid #ddd';
+  planItemE.style.borderRadius = '3px';
+  planItemE.style.width = '50%';
+  planItemE.style.boxShadow = '1px 1px 5px rgba(0, 0, 0, 0.08)';
 
-  const planDivE = document.createElement("div");
+  const planDivE = document.createElement('div');
   planItemE.appendChild(planDivE);
 
-  const planNameE = document.createElement("p");
+  const planNameE = document.createElement('p');
   planNameE.innerHTML = item.name;
   planDivE.appendChild(planNameE);
-  planNameE.style.display = "block";
-  planNameE.style.borderBottom = "1px solid #ddd";
-  planNameE.style.padding = "10px";
-  planNameE.style.margin = "0px";
-  planNameE.style.backgroundColor = "#f4f4f4";
-  planNameE.style.fontSize = "18px";
+  planNameE.style.display = 'block';
+  planNameE.style.borderBottom = '1px solid #ddd';
+  planNameE.style.padding = '10px';
+  planNameE.style.margin = '0px';
+  planNameE.style.backgroundColor = '#f4f4f4';
+  planNameE.style.fontSize = '18px';
 
-  const planContent = document.createElement("div");
+  const planContent = document.createElement('div');
   planDivE.appendChild(planContent);
-  planContent.style.display = "flex";
-  planContent.style.flexDirection = "column";
-  planContent.style.padding = "12px";
+  planContent.style.display = 'flex';
+  planContent.style.flexDirection = 'column';
+  planContent.style.padding = '12px';
 
-  const planPriceE = document.createElement("p");
+  const planPriceE = document.createElement('p');
   planPriceE.innerHTML = item.price;
   planContent.appendChild(planPriceE);
-  planPriceE.style.marginTop = "10px";
+  planPriceE.style.marginTop = '10px';
   planPriceE.style.fontWeight = 500;
 
-  const desListE = document.createElement("ul");
+  const desListE = document.createElement('ul');
   planContent.appendChild(desListE);
-  desListE.style.listStyle = "none";
+  desListE.style.listStyle = 'none';
   desListE.style.padding = 0;
 
   item.des.forEach(function (item) {
-    const desItemE = document.createElement("li");
+    const desItemE = document.createElement('li');
     desItemE.innerHTML = item;
     desListE.appendChild(desItemE);
-    desItemE.style.fontSize = "13px";
+    desItemE.style.fontSize = '13px';
   });
 
-  const planButtonE = document.createElement("button");
+  const planButtonE = document.createElement('button');
   planButtonE.innerHTML = item.button;
   planContent.appendChild(planButtonE);
-  planButtonE.style.marginTop = "15px";
-  planButtonE.style.padding = "8px";
+  planButtonE.style.marginTop = '15px';
+  planButtonE.style.padding = '8px';
   planButtonE.style.fontFamily = "'Roboto', sans-serif";
-  planButtonE.style.borderRadius = "3px";
-  planButtonE.style.color = "#fff";
-  planButtonE.style.border = "1px solid #0275d8";
-  planButtonE.style.backgroundColor = "#0275d8";
+  planButtonE.style.borderRadius = '3px';
+  planButtonE.style.color = '#fff';
+  planButtonE.style.border = '1px solid #0275d8';
+  planButtonE.style.backgroundColor = '#0275d8';
 });
 
 // Style for "Get started" button
-const outlineButton = document.querySelector("button");
-outlineButton.style.color = "#0275d8";
-outlineButton.style.backgroundColor = "transparent";
+const outlineButton = document.querySelector('button');
+outlineButton.style.color = '#0275d8';
+outlineButton.style.backgroundColor = 'transparent';
